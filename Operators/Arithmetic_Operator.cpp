a9d904cdbdfdc0a12f087277391485570b4564b2
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-11 20:49:30 +0530

    Operators

---
+++ Operators/Arithmetic_Operator.cpp
@@ -0,0 +1,22 @@
+   
+   // // **** ARITHMETIC OPERATOR  **** // //
+
+//    Two types of Arithmetic Operator 
+// 1. Unary operator
+// 2. Binary Operator
+
+// // // Binary Operator // // //
+// In Binary Operator we required two Operands with one Operator
+
+// + Addition           2+5
+// - Subtraction        8-3
+// * Multiply           5*4
+// / Division           10/2
+// % Modulus            5%3
+
+// Presedence of operator
+// 1. {* , / , %}  left to right Associativity
+// 2. {+ , -}  left to right Associativity
+
+// // // Unary Operator // // //
+// In unary operator we only required single operand and with operator
