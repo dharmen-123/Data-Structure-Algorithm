3d32e9f2d2f7491ad30517e12fcb25603f5acd13
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-11 16:01:32 +0530

    Square pattern

---
+++ Patterns_Problems/Square_Pattern.cpp
@@ -0,0 +1,37 @@
+    // // // Square Pattern // // //
+
+ // //   * * * * *   
+ // //   * * * * *       
+ // //   * * * * *   
+ // //   * * * * *       
+ // //   * * * * *       
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=5;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n;j++){
+//             cout<<" * ";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+ // /// // ///Dynamic Square pattern 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     char sym;
+//     cout<<"Enter the number of rows and column od square : ";
+//     cin>>n;
+//     cout<<"Type any symbol for square pattern :";
+//     cin>>sym;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<n;j++){
+//           cout<<" "<<sym<<" ";
+//         }    
+//         cout<<endl;
+//     }
+// }
