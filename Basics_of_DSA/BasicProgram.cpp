16d2f9377b3101b3a9c8fdba2800bc7667d8409d
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-13 20:42:02 +0530

    commit

--- Basics_of_DSA/BasicProgram.cpp
+++ Basics_of_DSA/BasicProgram.cpp
@@ -125,9 +125,15 @@ using namespace std;
 
 int main(){
 
-  int  n = 13;
-  int  a = n << 2;
-  cout<<a;
+  int  n = 135,rem = 0,sum = 0;
+  while(n>0){
+    rem = n%10;
+    sum = sum *10 + rem;
+    n = n/10;
+  
+  }
+  cout<<sum;
+  
 
   return 0;
 }
