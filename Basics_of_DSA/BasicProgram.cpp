b8706afe29d8084d8feb5fa4c324a2e65e86260b
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-13 14:13:44 +0530

    basic

--- Basics_of_DSA/BasicProgram.cpp
+++ Basics_of_DSA/BasicProgram.cpp
@@ -105,17 +105,29 @@
 // }
 
 // // //  Fibonacci Series // // 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int a=0 ,b=1 ,n=5,c;
+//     cout<<a<<endl<<b<<endl;
+//      for(int i=0;i<n-2;i++){
+//         c=a+b;
+//         cout<<c<<endl;
+//         a=b;
+//         b=c;        
+//      }
+
+// }
+
 #include<iostream>
 using namespace std;
 
 int main(){
-    int a=0 ,b=1 ,n=5,c;
-    cout<<a<<endl<<b<<endl;
-     for(int i=0;i<n-2;i++){
-        c=a+b;
-        cout<<c<<endl;
-        a=b;
-        b=c;        
-     }
 
+  int  n = 13;
+  int  a = n << 2;
+  cout<<a;
+
+  return 0;
 }
