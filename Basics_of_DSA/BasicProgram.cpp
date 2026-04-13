d87c022d015001e4873d15040af2727a2d92c6a2
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-13 20:45:47 +0530

    commmit

--- Basics_of_DSA/BasicProgram.cpp
+++ Basics_of_DSA/BasicProgram.cpp
@@ -117,23 +117,14 @@
 //         a=b;
 //         b=c;        
 //      }
-
 // }
+
+// Power of @ Question
 
 #include<iostream>
 using namespace std;
 
 int main(){
+      
 
-  int  n = 135,rem = 0,sum = 0;
-  while(n>0){
-    rem = n%10;
-    sum = sum *10 + rem;
-    n = n/10;
-  
-  }
-  cout<<sum;
-  
-
-  return 0;
 }
