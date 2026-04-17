10e31284c1a7c6f04744b0bfb5c9a1bb824ee187
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-17 18:48:53 +0530

    commit

---
+++ Array/Array_implement.cpp
@@ -0,0 +1,13 @@
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[5]={3,6,2,7,4};
+//     int n=sizeof(arr);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i];
+//     }
+//     return 0 ;
+// }
+
