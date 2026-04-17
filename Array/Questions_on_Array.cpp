fb7ae1a4a43a0a6a468ed1d4d9e9132963acfd88
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-17 22:23:30 +0530

    Search an Element

--- Array/Questions_on_Array.cpp
+++ Array/Questions_on_Array.cpp
@@ -20,19 +20,39 @@
 
     // //  Minimum element of array // // 
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
 
-int main(){
-    int arr1[5]={6,2,8,13,26};
-    int n=sizeof(arr1)/sizeof(arr1[0]);
-    int ans=INT_MAX;
-    for(int i=0;i<n;i++){
-        if(arr1[i]<ans){
-            ans=arr1[i];
-        }
-    }
-    cout<<"Minimum value is :"<<ans;
-return 0 ;
-}
+// int main(){
+//     int arr1[5]={6,2,8,13,26};
+//     int ans= INT_MAX ;
+//     int n=sizeof(arr1)/sizeof(arr1[0]);
+//     for(int i=0;i<n;i++){
+//         if(arr1[i]<ans){
+//             ans=arr1[i];
+//         }
+//     }
+//     cout<<"Minimum value is :"<<ans;
+// return 0 ;
+// }
+
+    // // // Search an Element in Array return index// // //
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int arr1[5]={6,2,8,13,26};
+//     int target=13;
+//     int n=sizeof(arr1)/sizeof(arr1[0]);
+//     for(int i=0;i<n;i++){
+//         if(arr1[i]==target){
+//             cout<<i;
+//             return 0;
+//         }
+//     }
+//     cout<<-1;
+
+// return 0 ;
+// }
