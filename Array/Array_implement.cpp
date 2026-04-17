3dc6524d622b0d237f2ce373c017a881de59be00
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-17 21:00:34 +0530

    Question on array

--- Array/Array_implement.cpp
+++ Array/Array_implement.cpp
@@ -1,18 +1,54 @@
 
+// // //          arr[5] = {5 ,12 ,56 ,23 ,8}
+// // //             ^      0  1   2   3   4  index
+// // //             |
+// // // array_name_with_size
+
+
 // #include<iostream>
 // using namespace std;
 
 // int main(){
 //     int arr[5]={3,6,2,7,4};
-//     int n=sizeof(arr);
+//     int n=sizeof(arr)/sizeof(arr[0]);
 //     for(int i=0;i<n;i++){
 //         cout<<arr[i];
 //     }
 //     return 0 ;
 // }
 
-// // //          arr[5] = {5 ,12 ,56 ,23 ,8}
-// // //             ^      0  1   2   3   4  index
-// // //             |
-// // // array_name_with_size
+    // // Assign a single value in  array // //
 
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int arr[6]={1};
+//     int n=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<n;i++){
+//         cout<<arr[i]<<endl;
+//     }
+
+// return 0 ;
+// }
+ 
+    // //  Taking Array value Input from user  // //
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"arr["<<i<<"]=";
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+
+return 0 ;
+}
--- Array/Questions_on_Array.cpp
+++ Array/Questions_on_Array.cpp
@@ -1,2 +1,19 @@
 
     // //  Maximum element of array // // 
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr1[5]={6,2,8,13,26};
+    int n=sizeof(arr1)/sizeof(arr1[0]);
+    int ans=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(arr1[i]>ans){
+            ans=arr1[i];
+        }
+    }
+    cout<<"Maximum value is :"<<ans;
+return 0 ;
+}
