d0487b44ade9113df3780dc347a2ba139e6d0685
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-20 21:06:42 +0530

    commit

---
+++ Sorting_Algorithm/BUBBLE_Sort.cpp
@@ -0,0 +1,20 @@
+
+    // //  ****** BUBBLE SORT ****** // // 
+
+// Bubble Sort is a comparison-based sorting algorithm.
+// It repeatedly compares adjacent elements and swaps them if they are in the wrong order. This process continues until the array is sorted.
+
+    // ****** Key Idea:
+
+// Larger elements “bubble up” to the end of the array after each pass.
+// After the first pass, the largest element is at the last position.
+// After the second pass, the second largest is at the second last position, and so on.
+
+// **** Working Steps ***** //
+
+// Compare the first two elements.
+// Swap if they are in the wrong order.
+// Move to the next pair and repeat.
+// After one full pass, the largest element is at the end.
+// Repeat for the remaining unsorted portion until all elements are sorted.
+
