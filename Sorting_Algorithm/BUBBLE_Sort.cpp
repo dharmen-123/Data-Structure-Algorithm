fc0482a5362fe27ad6a1cdee00ff999f4ef08754
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-20 22:43:31 +0530

    bubble sort

--- Sorting_Algorithm/BUBBLE_Sort.cpp
+++ Sorting_Algorithm/BUBBLE_Sort.cpp
@@ -18,3 +18,37 @@
 // After one full pass, the largest element is at the end.
 // Repeat for the remaining unsorted portion until all elements are sorted.
 
+#include<iostream>
+using namespace std;
+void BubbleSort(int arr[], int n){
+     
+    for(int i=0;i<n-1;i++){
+        bool check=true;
+        for(int j=0;j<=n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                check=false;
+            }
+        }
+        if(check==true){
+            break;
+        }
+    }
+}
+
+int main(){
+        int arr[1000];
+        int n;
+        cout<<"Enter the size of array: ";
+        cin>>n;
+        cout<<"Enter the array elements: ";
+        for(int i=0;i<n;i++){
+            cin>>arr[i];    
+        }
+        BubbleSort(arr,n);
+        for(int i=0;i<n;i++){
+            cout<<arr[i]<<" ";    
+        }
+
+return 0 ;
+}
