533e7f693d4d68de776acd208f6c42bb156f43e3
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-22 23:03:47 +0530

    commit

--- Searching_Algorithm/Binary_Search.cpp
+++ Searching_Algorithm/Binary_Search.cpp
@@ -10,3 +10,12 @@ It works only on sorted arrays/lists. The idea is to repeatedly divide the searc
 // If equal → found.
 // If smaller → search left half.
 // If larger → search right half.
+
+// Working Steps
+
+// Start with the entire sorted array.
+// Find the middle element.
+// If target equals middle → return index.
+// If target < middle → search left half.
+// If target > middle → search right half.
+// Repeat until found or interval is empty.
