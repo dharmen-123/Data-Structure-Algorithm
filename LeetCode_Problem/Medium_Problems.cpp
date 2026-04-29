cc66d9eb6aeb56e1a271e8f7549a3ee351071f35
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-29 19:22:06 +0530

    commit

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -54,39 +54,53 @@
 
         // // Q.33 Search in Rotated Sorted Array //
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int search(vector<int>& nums, int target) {
+//         int start=0,end=nums.size()-1, mid;
+//         while(start<=end){
+//            mid=start+(end-start)/2;
+//            if(nums[mid]==target){
+//                 return mid;
+//            }
+//            else if(nums[mid]>=nums[0]){
+//                 if(nums[start]<=target && nums[mid]>target){
+//                         end=mid-1;
+//                 }
+//                 else{
+//                         start=mid+1;
+//                 }
+//            }
+//            else{
+//                 if(nums[end]>=target && nums[mid]<target){
+//                         start=mid+1;
+//                 }
+//                 else{
+//                         end=mid-1;
+//                 }
+//            }
+//         }
+//         return -1;
+//     }
+// int main(){
+//         vector<int>nums={4,5,6,7,0,1,2};
+//         int target=3;
+//         cout<<search(nums, target);
+
+// return 0 ;
+// }
+
+           // // Q.1539  Kth Missing Positive Number   //
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int search(vector<int>& nums, int target) {
-        int start=0,end=nums.size()-1, mid;
-        while(start<=end){
-           mid=start+(end-start)/2;
-           if(nums[mid]==target){
-                return mid;
-           }
-           else if(nums[mid]>=nums[0]){
-                if(nums[start]<=target && nums[mid]>target){
-                        end=mid-1;
-                }
-                else{
-                        start=mid+1;
-                }
-           }
-           else{
-                if(nums[end]>=target && nums[mid]<target){
-                        start=mid+1;
-                }
-                else{
-                        end=mid-1;
-                }
-           }
-        }
-        return -1;
+int findKthPositive(vector<int>& arr, int k) {
+        
     }
 int main(){
-        vector<int>nums={4,5,6,7,0,1,2};
-        int target=3;
-        cout<<search(nums, target);
+
 
 return 0 ;
 }
