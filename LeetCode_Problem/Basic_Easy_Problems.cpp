f820d20184aec6caf5aa2430bb66a5e3629ae106
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-29 20:02:18 +0530

    commit

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -166,28 +166,28 @@
 
  // // Q.1539  Kth Missing Positive Number   //
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int findKthPositive(vector<int>& arr, int k) {
-     int start=0, end=arr.size()-1 , mid, ans=arr.size();
-     while(start<=end){
-        mid=start+(end-start)/2;
-        if(arr[mid]-mid-1>=k){
-                ans=mid;
-                end=mid-1;
-        }
-        else{
-                start=mid+1;
-        }
-     }   
-     return ans+k;
-    }
-int main(){
-        vector<int>arr={1,2,3,4};
-        int k=2;
-        cout<<findKthPositive(arr,k);
-return 0 ;
-}
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int findKthPositive(vector<int>& arr, int k) {
+//      int start=0, end=arr.size()-1 , mid, ans=arr.size();
+//      while(start<=end){
+//         mid=start+(end-start)/2;
+//         if(arr[mid]-mid-1>=k){
+//                 ans=mid;
+//                 end=mid-1;
+//         }
+//         else{
+//                 start=mid+1;
+//         }
+//      }   
+//      return ans+k;
+//     }
+// int main(){
+//         vector<int>arr={1,2,3,4};
+//         int k=2;
+//         cout<<findKthPositive(arr,k);
+// return 0 ;
+// }
 
 
