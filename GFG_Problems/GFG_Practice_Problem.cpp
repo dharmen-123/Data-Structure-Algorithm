71efd43ae5a73157f8ee850778349af8f0fe6604
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-04-30 17:03:07 +0530

    commit

--- GFG_Problems/GFG_Practice_Problem.cpp
+++ GFG_Problems/GFG_Practice_Problem.cpp
@@ -1,42 +1,83 @@
 
- // // Q. Allocation minimumn Number of Pages    //
+     // // Q. Allocation minimumn Number of Pages    //
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int findPages(vector<int> &arr, int K) {
-        int N=arr.size();
-        if(K>N)
-         return -1;
-        int start=0,end=0,mid, ans;
-        for(int i=0;i<N;i++){
-                start=max(start,arr[i]);
-                end+=arr[i];
-        }
-        while(start<=end){
-           mid=(start+end)/2;
-           int pages=0, count=1;
-           for(int i=0;i<N;i++){
-                pages+=arr[i];
-                if(pages>mid){
-                     count++;
-                     pages=arr[i];
-                }
-           }
-           if(count<=K){
-                ans=mid;
-                end=mid-1;
-           }
-           else{
-                start=mid+1;
-           }
-        }
-        return ans;
-    }
-int main(){
-        vector<int>arr={12,34,67,90};
-        int m=2;
-        cout<<findPages(arr,m);
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int findPages(vector<int> &arr, int K) {
+//         int N=arr.size();
+//         if(K>N)
+//          return -1;
+//         int start=0,end=0,mid, ans;
+//         for(int i=0;i<N;i++){
+//                 start=max(start,arr[i]);
+//                 end+=arr[i];
+//         }
+//         while(start<=end){
+//            mid=(start+end)/2;
+//            int pages=0, count=1;
+//            for(int i=0;i<N;i++){
+//                 pages+=arr[i];
+//                 if(pages>mid){
+//                      count++;
+//                      pages=arr[i];
+//                 }
+//            }
+//            if(count<=K){
+//                 ans=mid;
+//                 end=mid-1;
+//            }
+//            else{
+//                 start=mid+1;
+//            }
+//         }
+//         return ans;
+//     }
+// int main(){
+//         vector<int>arr={12,34,67,90};
+//         int m=2;
+//         cout<<findPages(arr,m);
+// return 0 ;
+// }
 
-return 0 ;
-}
+        // // Q.  Painter's Partition Problem //
+    
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int minTime(vector<int>& arr, int k) {
+//         int N=arr.size();
+//         if(k>N)
+//          return -1;
+//         int start=0,end=0,mid, ans;
+//         for(int i=0;i<N;i++){
+//                 start=max(start,arr[i]);
+//                 end+=arr[i];
+//         }
+//         while(start<=end){
+//            mid=(start+end)/2;
+//            int pages=0, count=1;
+//            for(int i=0;i<N;i++){
+//                 pages+=arr[i];
+//                 if(pages>mid){
+//                      count++;
+//                      pages=arr[i];
+//                 }
+//            }
+//            if(count<=k){
+//                 ans=mid;
+//                 end=mid-1;
+//            }
+//            else{
+//                 start=mid+1;
+//            }
+//         }
+//         return ans;       
+//     }
+// int main(){
+//     vector<int>arr={5, 10, 30, 20, 15};
+//     int k=3;
+//     cout<<minTime(arr,k);
+
+// return 0 ;
+// }
