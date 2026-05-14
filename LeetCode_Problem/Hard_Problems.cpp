b2ded1cf9069b960e7b69e026b981541da181ae7
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-14 21:50:04 +0530

    Trapping Rain water problem

--- LeetCode_Problem/Hard_Problems.cpp
+++ LeetCode_Problem/Hard_Problems.cpp
@@ -1,2 +1,43 @@
 
-    /* KOKO  */
+    /* // // Q.42 Trapping Rain Water // // */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int trap(vector<int>& height) {
+        int n=height.size(),water=0;
+        int leftmax=0, rightmax=0,maxheight=height[0],index=0;
+        //Max height of building
+        for(int i=1;i<n;i++){
+            if(maxheight<height[i]){
+                maxheight=height[i];
+                index=i;
+            }
+        }
+        //Left part
+            for(int i=0;i<index;i++){
+                if(leftmax>height[i]){
+                    water+=leftmax-height[i];
+                }
+                else{
+                    leftmax=height[i];
+                }
+            }
+        // Right part
+          for(int i=n-1;i>index;i--){
+                if(rightmax>height[i]){
+                    water+=rightmax-height[i];
+                }
+                else{
+                    rightmax=height[i];
+                }
+            }
+        return water;
+    } 
+
+int main(){
+        vector<int>height={0,1,0,2,1,0,1,3,2,1,2,1};
+        cout<<"Total water : "<<trap(height);
+
+return 0 ;
+}
