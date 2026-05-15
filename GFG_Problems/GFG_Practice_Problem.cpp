6c0813b81564f2d69dac542a5810a9c489ba3ea7
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-15 20:39:40 +0530

    commit

--- GFG_Problems/GFG_Practice_Problem.cpp
+++ GFG_Problems/GFG_Practice_Problem.cpp
@@ -208,3 +208,35 @@
 
 // return 0 ;
 // }
+
+        /*  // Triplet Sum in Array  //  */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+ bool hasTripletSum(vector<int> &arr, int target) {
+        int n=arr.size(),X=0,start , end;
+        for(int i=0;i<n-2;i++){
+            start=i+1 , end=n-1;
+            X=target-arr[0];
+            while(start<end){
+                if(arr[start]+arr[end]==X){
+                        return 1;
+                }
+                else if(arr[start]+arr[end]>X){
+                        end--;
+                }
+                else{
+                        start++;
+                }
+            } 
+        }
+        return 0;
+    }
+int main(){
+        vector<int>arr={1, 4, 45, 6, 10, 2};
+        int target=13;
+        cout<<hasTripletSum(arr,target);
+
+return 0 ;
+}
