eb2fce6ea55e6b93f1ed2b8a49442318d83b4225
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-16 21:25:24 +0530

    2D vector

---
+++ Standard_Template_Library/Vector_in_2DArray.cpp
@@ -0,0 +1,21 @@
+
+        /* // Vector in 2D Array // */
+
+// #include<iostream>
+// #include"vector"
+// #include"algorithm"
+// using namespace std;
+
+// int main(){
+//     vector<vector<int>>matrix(3,vector<int>(4,1));
+//     for(auto k:matrix){
+//         for(auto v:k){
+//             cout<<v<<" ";
+//         }
+//         cout<<endl;
+//     }
+//     cout<<"Number of Rows : "<<matrix.size()<<endl;
+//     cout<<"Number of Column : "<<matrix[0].size();
+
+// return 0 ;
+// }
