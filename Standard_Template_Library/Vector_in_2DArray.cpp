c06277b48a83f071d28e0e2b0f72d88c4aa9b00a
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-16 22:32:35 +0530

    commit

--- Standard_Template_Library/Vector_in_2DArray.cpp
+++ Standard_Template_Library/Vector_in_2DArray.cpp
@@ -19,3 +19,32 @@
 
 // return 0 ;
 // }
+
+        /* // Dynamic size and data of 2D Vector   // */
+
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+
+// int main(){
+//     int n , m;
+//     cout<<"Enter the number of rows: ";
+//     cin>>n;
+//     cout<<"Enter the number of column: ";
+//     cin>>m;
+//     vector<vector<int>>mat(n,vector<int>(m));
+//     cout<<"Enter the values of matrix :"<<endl;
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cin>>mat[i][j];
+//         }    
+//     }
+//     for(int i=0;i<n;i++){
+//         for(int j=0;j<m;j++){
+//             cout<<mat[i][j]<<" ";
+//         }    
+//         cout<<endl;
+//     }
+// return 0 ;
+// }
