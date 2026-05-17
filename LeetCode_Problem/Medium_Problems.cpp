5d98259a774521683afabaf16f4de642a6ab2c3c
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-17 08:54:05 +0530

    Spiral Matrix Problem

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -165,3 +165,42 @@
 
 // return 0 ;
 // }
+
+        /*  // Q,54 Sprial Matrix    */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+vector<int>spiralOrder(vector<vector<int> >& matrix) {
+        int row=matrix.size(), col = matrix[0].size();
+        int top=0 ,right=col-1 ,bottom=row-1 ,left=0;
+        vector<int>v;
+        while(top<=bottom && left<=right){
+        for(int i=left;i<=right;i++){
+                v.push_back(matrix[top][i]);
+        }
+        top++;
+        for(int i=top;i<=bottom;i++){
+                v.push_back(matrix[i][right]);
+        }
+        right--;
+        for(int i=right;i>=left;i--){
+                v.push_back(matrix[bottom][i]);
+        }
+        bottom--;
+        for(int i=bottom;i>=top;i--){
+                v.push_back(matrix[i][left]);
+        }
+        left++;
+        }
+        return v;
+}
+int main(){
+        vector<vector<int>>matrix={{1,2,3},{4,5,6},{7,8,9}};
+        vector<int>ans=spiralOrder(matrix);
+        for(auto k:ans){
+                cout<<k<<" ";
+        }
+return 0 ;
+}
