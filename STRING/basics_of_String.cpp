1ed72513aa2305d697690a1b7328614b79244cee
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-20 17:13:32 +0530

    String

---
+++ STRING/basics_of_String.cpp
@@ -0,0 +1,26 @@
+
+        /*  // STRING //  */
+
+    /* C Style string  ( Charater String) */
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char arr[]={'a','p','p','l','e'};
+//     for(int i=0;i<5;i++)
+//     cout<<arr[i];
+
+// return 0 ;
+// }
+
+////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+int main(){
+    char str[20];
+    cin>>str;
+    cout<<str;
+    
+return 0 ;
+}
