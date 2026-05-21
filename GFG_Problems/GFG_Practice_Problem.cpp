e882047b689510382747c0dc83f65c2ead1fe08c
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-21 21:54:55 +0530

    Sort a String

--- GFG_Problems/GFG_Practice_Problem.cpp
+++ GFG_Problems/GFG_Practice_Problem.cpp
@@ -472,11 +472,23 @@
 #include<vector>
 using namespace std;
 string sortString(string &s) {
-        // code here
-        
+        vector<int>alpha(26,0);
+        for(int i=0;i<s.size();i++){
+                alpha[s[i]-'a']++;
+        }
+        string ans;
+        for(int i=0;i<26;i++){
+                char c='a'+i;
+                while(alpha[i]){
+                   ans+=c;
+                   alpha[i]--;
+                }
+        }
+        return ans;
     }
 int main(){
-
+        string s = "edcab";
+        cout<<sortString(s);
 
 return 0 ;
 }
