30e783b3ebf1e591b8742ff38810df8f6a376e9c
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-23 09:51:42 +0530

    commit

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -415,49 +415,49 @@
 
             /* // Q.415. Add Strings // */
 
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-string add(string num1, string num2) {
-        string ans;
-        int index1=num1.size()-1, index2=num2.size()-1;
-        int sum, carry=0;
-        //index>=0
-        while(index2>=0){
-            sum=(num1[index1]-'0')+(num2[index2]-'0')+carry;
-            carry=sum/10;
-            char c='0'+sum%10;
-            ans+=c;
-            index2-- , index1--;
-        }
-        //index1>=0
-        while(index1>=0){
-            sum=(num1[index1]-'0')+carry;
-            carry=sum/10;
-            char c='0'+sum%10;
-            ans+=c;
-            index1--;
-        }
-        if(carry){
-            ans+='1';
-        }
-        reverse(ans.begin(),ans.end());
+// #include<iostream>
+// #include<vector>
+// #include<algorithm>
+// using namespace std;
+// string add(string num1, string num2) {
+//         string ans;
+//         int index1=num1.size()-1, index2=num2.size()-1;
+//         int sum, carry=0;
+//         //index>=0
+//         while(index2>=0){
+//             sum=(num1[index1]-'0')+(num2[index2]-'0')+carry;
+//             carry=sum/10;
+//             char c='0'+sum%10;
+//             ans+=c;
+//             index2-- , index1--;
+//         }
+//         //index1>=0
+//         while(index1>=0){
+//             sum=(num1[index1]-'0')+carry;
+//             carry=sum/10;
+//             char c='0'+sum%10;
+//             ans+=c;
+//             index1--;
+//         }
+//         if(carry){
+//             ans+='1';
+//         }
+//         reverse(ans.begin(),ans.end());
     
-    return ans;
+//     return ans;
 
-    }
-    string addStrings(string num1, string num2) {
-        if(num1.size()>num2.size())
-            return add(num1,num2);
-        else
-            return add(num2,num1);
+//     }
+//     string addStrings(string num1, string num2) {
+//         if(num1.size()>num2.size())
+//             return add(num1,num2);
+//         else
+//             return add(num2,num1);
 
-    }
-int main(){
-    // string num1 = "11", num2 = "123";
-    string num1 = "456", num2 = "77";
-    cout<<addStrings(num1,num2);
+//     }
+// int main(){
+//     // string num1 = "11", num2 = "123";
+//     string num1 = "456", num2 = "77";
+//     cout<<addStrings(num1,num2);
 
-return 0 ;
-}
+// return 0 ;
+// }
--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -494,3 +494,28 @@
 // }
 
 
+        /* // Q.3. Longest Substring Without Repeating Characters  // */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int lengthOfLongestSubstring(string s) {
+        vector<bool>count(256,0); 
+        int first =0, second =0, len=0;
+        while(second<s.size()){
+                while(count[s[second]]){
+                        count[s[first]]=0;
+                        first++;
+                }
+                count[s[second]]=1;
+                len=max(len,second-first+1);
+                second++;
+        }   
+        return len;
+    }
+int main(){
+        string s = "pwwkew";
+        cout<<lengthOfLongestSubstring(s);
+
+return 0 ;
+}
