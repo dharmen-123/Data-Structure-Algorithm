3daa8be943c3d0ec3c4456c7c68c43af84c64204
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-05-27 21:17:40 +0530

    commit

--- POINTER/Pointer_basics.cpp
+++ POINTER/Pointer_basics.cpp
@@ -1,16 +1,53 @@
 
             /* // Pointer  //  */
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
-    int a=10;
-    int *ptr=&a;
-    cout<<a<<endl;      //output - 10
-    cout<<&a<<endl;     //         0x61ff08 
-    cout<<ptr<<endl;    //         0x61ff08
-    cout<<*ptr;         //         10
+// int main(){
+//     int a=10;
+//     int *ptr=&a;
+//     cout<<a<<endl;      //output - 10
+//     cout<<&a<<endl;     //         0x61ff08 
+//     cout<<ptr<<endl;    //         0x61ff08
+//     cout<<*ptr;         //         10
 
-return 0 ;
-}
+// return 0 ;
+// }
+
+    /* // Size of Pointer // */
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     int c=20;
+//     int *ptr1=&c;
+//     cout<<sizeof(ptr1)<<endl;
+//     char a='n';
+//     char *ptr2 = &a;
+//     cout<<sizeof(ptr2)<<endl;
+//     float m=2.5;
+//     float *ptr3 = &m;
+//     cout<<sizeof(ptr3)<<endl;
+
+// return 0 ;
+// }
+
+        /* // Pointer point next // */
+    
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a=20;
+//     int *ptr=&a;
+//     cout<<ptr<<endl;        // 0x61ff08
+//     cout<<*ptr<<endl;       // 20
+//     int b=30;
+//     ptr=&b;
+//     cout<<ptr<<endl;        // 0x61ff04
+//     cout<<*ptr<<endl;       // 30
+
+// return 0 ;
+// }
+
