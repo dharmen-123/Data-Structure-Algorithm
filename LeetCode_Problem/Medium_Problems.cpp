af28245a6af113c21c3c86e6bad795ac8710c844
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-01 22:36:51 +0530

    commit

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -522,41 +522,44 @@
 
         /* // Q.151. Reverse Words in a String  //  */
 
-#include<iostream>
-#include<stack>
-#include<vector>
-using namespace std;
-string reverseWords(string s) {
-        stack<string>res;
-        string word;
-        for(int i=0;i<s.size();i++){
-                if(s[i]!=' '){
-                   word+=s[i];
-                }
-                else{
-                    if(word.size()!=0){
-                        res.push(word);
-                        word.clear();
-                    }
-                }
-        }
-        if(word.size()!=0){
-            res.push(word);
-            word.clear();
-        }
-        while(!res.empty()){
-            word+=res.top();
-            word+=" ";
-            res.pop();
-        }
-        return word;
-    }
-int main(){
-        string s="the sky is blue";
-        cout<<reverseWords(s);
+// #include<iostream>
+// #include<stack>
+// #include<vector>
+// using namespace std;
+// string reverseWords(string s) {
+//         stack<string>res;
+//         string word;
+//         for(int i=0;i<s.size();i++){
+//                 if(s[i]!=' '){
+//                    word+=s[i];
+//                 }
+//                 else{
+//                     if(word.size()!=0){
+//                         res.push(word);
+//                         word.clear();
+//                     }
+//                 }
+//         }
+//         if(word.size()!=0){
+//             res.push(word);
+//             word.clear();
+//         }
+//         while(!res.empty()){
+//             word+=res.top();
+//             res.pop();
+//             if(!res.empty()) {
+//             word += " ";
+//           }
 
-return 0 ;
-}
+//         }
+//         return word;
+//     }
+// int main(){
+//         string s="the sky is blue";
+//         cout<<reverseWords(s);
+
+// return 0 ;
+// }
     
     
         /* // Q.686. Repeated String Match  //  */
