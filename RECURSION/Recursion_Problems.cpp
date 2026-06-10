5a2292c832f6c9ff40f886259c8cb5ac4aca81e3
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-10 22:43:12 +0530

    commit

--- RECURSION/Recursion_Problems.cpp
+++ RECURSION/Recursion_Problems.cpp
@@ -3,18 +3,52 @@
 
     /* // Print 1 to N number // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// void Print(int n){
+//         if(n==0){
+//             return ;
+//         }
+//         Print(n-1);
+//         cout<<n<<" ";
+// }
+// int main(){
+//     int n=20;
+//     Print(n);
+// return 0 ;
+// }
+
+    /* // Print 1 to N Even number // */
+
+// #include<iostream>
+// using namespace std;
+// void Printeven(int n){
+//     if(n==0){
+//         return ;
+//     }
+//     Printeven(n-1);
+//     if(n%2==0){
+//         cout<<n<<" ";
+//     }
+// }
+// int main(){
+//     int n=20;
+//     Printeven(n);
+// return 0 ;
+// }
+
+        /* // Factorial of any Number // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void Print(int n){
-        if(n==0){
-            return ;
-        }
-        Print(n-1);
-        cout<<n<<" ";
+void factorial(int n){
+    
 }
 int main(){
-    int n=20;
-    Print(n);
+    int n=5;
+    factorial(n);
+
 return 0 ;
 }
