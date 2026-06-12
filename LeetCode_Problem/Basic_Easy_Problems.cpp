ebc7169839ff428779c2041feccfec1547a3a7de
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-12 19:50:25 +0530

    Count Operations to Obtain Zero

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -1069,3 +1069,28 @@
 // return 0 ;
 // }
 
+        /* // Q.2169. Count Operations to Obtain Zero // */
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int counter(int num1, int num2, int count){
+//         if(num1==0 || num2==0){
+//                 return count;
+//         }
+//         if(num1>=num2){
+//                return counter(num1-=num2,num2,count+1);
+//         }
+//         else{
+//                return counter(num1,num2-=num1,count+1);
+//         }
+// }
+// int countOperations(int num1, int num2) {
+//         return counter(num1,num2,0);
+//     }
+// int main(){
+//         int num1=2, num2=3;
+//         cout<<countOperations(num1,num2);
+
+// return 0 ;
+// }
