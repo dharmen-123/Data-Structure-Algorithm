d633eb4cd6d1e3dc098abc0e4c8848ff36901d03
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-22 21:09:48 +0530

    RECURSION IN ARRAY

--- GFG_Problems/GFG_Practice_Problem.cpp
+++ GFG_Problems/GFG_Practice_Problem.cpp
@@ -530,3 +530,26 @@
 // return 0 ;
 // }
 
+        /*  // Q.GCD of two numbers  // */
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int gcd(int a, int b) {
+//       if(a<b){
+//         swap(a,b);
+//       }
+//       int c;
+//       while(b!=0){
+//         c=b;  
+//         b=a%b;
+//         a=c;
+//       }
+//    return a;
+//     }
+// int main(){
+//         int a=20,b=28;
+//         cout<<gcd(a,b);
+
+// return 0 ;
+// }
---
+++ RECURSION/Recursion_in_Array.cpp
@@ -0,0 +1,20 @@
+
+            /*  RECURSION IN ARRAY  */
+
+    /* Print the Array Elements */
+
+// #include<iostream>
+// using namespace std;
+// void Display(int arr[], int index,int n){
+//       if(index==n){
+//         return;
+//       }
+//         cout<<arr[index]<<" ";
+//         Display(arr,index+1,n);
+// }
+// int main(){
+//     int arr[]={4,8,23,5,9};
+//     Display(arr,0,5);
+
+// return 0 ;
+// }
