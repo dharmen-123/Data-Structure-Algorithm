028e77fd1b69020845559424c360fc3561dc5a84
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-23 21:53:34 +0530

    commit

--- RECURSION/Recursion_in_Array.cpp
+++ RECURSION/Recursion_in_Array.cpp
@@ -18,3 +18,20 @@
 
 // return 0 ;
 // }
+
+        /* Sum of All Element */
+
+// #include<iostream>
+// using namespace std;
+// int SumAll(int arr[],int index,int n){
+//         if(index==n){
+//             return 0;
+//         }
+//         return arr[index]+SumAll(arr,index+1,n);
+// }
+// int main(){
+//     int arr[]={2,4,6,8,10};
+//     cout<<SumAll(arr,0,5);
+
+// return 0 ;
+// }
---
+++ Sorting_Algorithm/MERGE_Sort.cpp
@@ -0,0 +1,36 @@
+
+            /*  // ******  MERGE SORT ****** //  */
+/*
+Merge Sort is a divide-and-conquer sorting algorithm.
+It works by recursively splitting the array into halves, sorting each half, and then merging them back together in sorted order.
+
+Key idea: Divide → Sort → Merge.
+
+    ** Working Steps **
+
+* Divide the array into two halves.
+* Recursively sort each half.
+* Merge the two sorted halves into one sorted array.
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+return 0 ;
+}
+
+
+
+/*
+***** Complexity Analysis ******
+
+* Best Case: O(n log n)
+* Worst Case: O(n log n)
+* Average Case: O(n log n)
+* Space Complexity: O(n) (extra space for merging)
+* Stable: Yes (preserves order of equal elements).
+*/
