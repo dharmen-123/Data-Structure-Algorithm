60ce83d8c70197b4f1f7041f0feef40767ad6d17
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-06-27 18:00:43 +0530

    Permutation II

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -947,31 +947,69 @@
 // return 0 ;
 // }
 
-                /* // Q.31. Next Permutation  // */
+                /* // Q.47. Permutations II  // */
 
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;        
-        while (i >= 0 && nums[i] >= nums[i+1]) {
-            i--;
-        }        
-        if (i >= 0) {
-            int j = n - 1;
-            while (nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums[i], nums[j]);
+void Combination(vector<int>& nums,vector<vector<int>>& ans,int index){
+        if(nums.size()==index){
+            ans.push_back(nums);
+            return ;
         }
-        reverse(nums.begin() + i + 1, nums.end());
+        for(int i=index;i<nums.size();i++){
+             if(nums[i]!=nums[i+1]){
+                swap(nums[index],nums[i]);
+                Combination(nums,ans,index+1);
+                swap(nums[index],nums[i]);
+                }
+        }
+}
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>>ans;
+        Combination(nums,ans,0);
+        return ans;
     }
 int main(){
-        vector<int>nums={1,2,3};
-        nextPermutation(nums);
-        for(auto k:nums){
-                cout<<k<<" ";
+        vector<int>nums={1,1,2};
+        vector<vector<int>>result=permuteUnique(nums);
+        for(auto k:result){
+                cout<<"[";
+                for(auto a:k){
+                   cout<<a<<" ";
+                }
+                cout<<"],";
         }
+  
 return 0 ;
 }
+
+                /* // Q.31. Next Permutation  // */
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// void nextPermutation(vector<int>& nums) {
+//         int n = nums.size();
+//         int i = n - 2;        
+//         while (i >= 0 && nums[i] >= nums[i+1]) {
+//             i--;
+//         }        
+//         if (i >= 0) {
+//             int j = n - 1;
+//             while (nums[j] <= nums[i]) {
+//                 j--;
+//             }
+//             swap(nums[i], nums[j]);
+//         }
+//         reverse(nums.begin() + i + 1, nums.end());
+//     }
+// int main(){
+//         vector<int>nums={1,2,3};
+//         nextPermutation(nums);
+//         for(auto k:nums){
+//                 cout<<k<<" ";
+//         }
+// return 0 ;
+// }
+
