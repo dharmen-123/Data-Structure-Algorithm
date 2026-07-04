bbe23d5609c5651718e6169323752b957dcb9748
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-07-04 11:01:22 +0530

    Can Place Flowers

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -1135,31 +1135,32 @@
 
         /* // 238. Product of Array Except Self //  */
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-vector<int> productExceptSelf(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> result(n, 1);
-    int prefix = 1;
-    for(int i = 0; i < n; i++) {
-        result[i] = prefix;
-        prefix *= nums[i];
-    }
-    int suffix = 1;
-    for(int i = n-1; i >= 0; i--) {
-        result[i] *= suffix;
-        suffix *= nums[i];
-    }
-    return result;
-    }
-int main(){
-//     vector<int>nums = {1,2,3,4};
-    vector<int>nums = {-1,1,0,-3,3};
-//     vector<int>nums = {-1,1,0,-3,0};
-    vector<int>result=productExceptSelf(nums);
-    for(auto k:result){
-        cout<<k<<" ";
-    }
-return 0 ;
-}
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int> productExceptSelf(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> result(n, 1);
+//     int prefix = 1;
+//     for(int i = 0; i < n; i++) {
+//         result[i] = prefix;
+//         prefix *= nums[i];
+//     }
+//     int suffix = 1;
+//     for(int i = n-1; i >= 0; i--) {
+//         result[i] *= suffix;
+//         suffix *= nums[i];
+//     }
+//     return result;
+//     }
+// int main(){
+// //     vector<int>nums = {1,2,3,4};
+//     vector<int>nums = {-1,1,0,-3,3};
+// //     vector<int>nums = {-1,1,0,-3,0};
+//     vector<int>result=productExceptSelf(nums);
+//     for(auto k:result){
+//         cout<<k<<" ";
+//     }
+// return 0 ;
+// }
+
