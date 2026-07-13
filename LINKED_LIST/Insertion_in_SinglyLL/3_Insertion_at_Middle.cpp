1ad2f98ee6363e6514feb73ccc920bc83baa773d
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-07-13 23:32:18 +0530

    commit

--- LINKED_LIST/Insertion_in_SinglyLL/3_Insertion_at_Middle.cpp
+++ LINKED_LIST/Insertion_in_SinglyLL/3_Insertion_at_Middle.cpp
@@ -5,6 +5,60 @@
 
 /* // Insertion at the Particular Position  of Linked List  // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node* next;
+//       Node(int val){
+//         data=val;
+//         next=NULL;
+//       }
+// };
+// Node* CreateLL(vector<int>& arr, int index){
+//         /* Base condition */    
+//         if(index==arr.size()){
+//             return NULL;
+//         }
+//         Node *temp;
+//         temp=new Node(arr[index]);
+//         temp->next=CreateLL(arr,index+1);
+  
+//         return temp;
+// }
+
+// int main(){
+//         Node *Head=NULL;
+//         vector<int>arr={2,4,6,8,10};
+//         Head=CreateLL(arr,0);
+
+//         /* Insert Node at Particular position */
+//         int pos=3,val=7;
+        
+//         Node *temp1=Head;
+//         pos--;
+//         while(pos--){
+//             temp1=temp1->next;
+//         }
+//         Node *temp2=new Node(val);
+//         temp2->next=temp1->next;
+//         temp1->next=temp2;
+
+//         /* Print the list */
+//         Node *temp=Head;
+//         while(temp!=NULL){
+//             cout<<temp->data<<" ";
+//             temp=temp->next;
+//         }
+// return 0 ;
+// }
+
+
+/* // Insertion at the Particular Position of Linked List
+            USING RECURSION               // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -28,7 +82,13 @@ Node* CreateLL(vector<int>& arr, int index){
   
         return temp;
 }
-
+Node* InsertMiddle(int pos,Node* temp){
+      if(pos==1){
+         return temp;
+      }
+       temp=temp->next;
+         
+}
 int main(){
         Node *Head=NULL;
         vector<int>arr={2,4,6,8,10};
@@ -38,6 +98,7 @@ int main(){
         int pos=3,val=7;
         
         Node *temp1=Head;
+
         pos--;
         while(pos--){
             temp1=temp1->next;
@@ -54,3 +115,5 @@ int main(){
         }
 return 0 ;
 }
+
+
--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -1678,31 +1678,31 @@
 
         /* // Q.1331. Rank Transform of an Array  // */
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> temp = arr;
-        sort(temp.begin(), temp.end());
-        unordered_map<int, int>rank;
-        int r = 1;
-        for (int x : temp){
-            if (rank.find(x) == rank.end()){
-                rank[x] = r++;
-            }
-        }
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = rank[arr[i]];
-        }
-        return arr;
-}
-int main(){
-        vector<int>arr = {40,10,20,30};
-        // vector<int>arr = {100,100,100};
-        vector<int>result=arrayRankTransform(arr);
-        for(auto k:result){
-                cout<<k<<" ";
-        }
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// vector<int> arrayRankTransform(vector<int>& arr) {
+//         vector<int> temp = arr;
+//         sort(temp.begin(), temp.end());
+//         unordered_map<int, int>rank;
+//         int r = 1;
+//         for (int x : temp){
+//             if (rank.find(x) == rank.end()){
+//                 rank[x] = r++;
+//             }
+//         }
+//         for (int i = 0; i < arr.size(); i++) {
+//             arr[i] = rank[arr[i]];
+//         }
+//         return arr;
+// }
+// int main(){
+//         vector<int>arr = {40,10,20,30};
+//         // vector<int>arr = {100,100,100};
+//         vector<int>result=arrayRankTransform(arr);
+//         for(auto k:result){
+//                 cout<<k<<" ";
+//         }
 
-return 0 ;
-}
+// return 0 ;
+// }
