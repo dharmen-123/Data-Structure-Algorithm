c5f221fa8f65eaf943e7ef7a88404e9a0fe5b15d
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-07-13 20:20:11 +0530

    Insert Node at END using Recursion

--- LINKED_LIST/Insertion_in_SinglyLL/2_Insertion_at_END.cpp
+++ LINKED_LIST/Insertion_in_SinglyLL/2_Insertion_at_END.cpp
@@ -48,6 +48,54 @@
 /* // Insertion at the End of the Linked List 
             When List is NOT empty          // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Node{
+//     public:
+//      int data;
+//      Node *next;
+
+//      Node(int val){
+//         data=val;
+//         next=NULL;
+//      }
+// };
+// int main(){
+//         Node *Head=new Node(10);
+//         Node *node2=new Node(7);
+//         Node *node3=new Node(5);
+//         Head->next=node2;
+//         node2->next=node3;
+//         node3->next=NULL;
+//         Node *Tail=node3;
+
+//         vector<int>arr={3,5,8};
+//         for(int i=0;i<arr.size();i++){
+//              /* Linked List is empty */
+//              if(Head==NULL){
+//                 Head=new Node(arr[i]);
+//                 Tail=Head;
+//              }
+//              /* Linked List consist some node */
+//              else{
+//                  Tail->next=new Node(arr[i]);
+//                  Tail=Tail->next;
+//              }
+//         }
+//         /* Print the linked List */
+//         Node *temp=Head;
+//         while(temp!=NULL){
+//             cout<<temp->data<<" ";
+//             temp=temp->next;
+//         }
+// return 0 ;
+// }
+
+
+/* // Insertion at the End of the Linked List 
+            USING THE RECURSION          // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -61,34 +109,30 @@ class Node{
         next=NULL;
      }
 };
-int main(){
-        Node *Head=new Node(10);
-        Node *node2=new Node(7);
-        Node *node3=new Node(5);
-        Head->next=node2;
-        node2->next=node3;
-        node3->next=NULL;
-        Node *Tail=node3;
-
-        vector<int>arr={3,5,8};
-        for(int i=0;i<arr.size();i++){
-             /* Linked List is empty */
-             if(Head==NULL){
-                Head=new Node(arr[i]);
-                Tail=Head;
-             }
-             /* Linked List consist some node */
-             else{
-                 Tail->next=new Node(arr[i]);
-                 Tail=Tail->next;
-             }
+Node* CreateLL(vector<int>& arr, int index){
+        /* Base condition */    
+        if(index==arr.size()){
+            return NULL;
         }
-        /* Print the linked List */
+        Node *temp;
+        temp=new Node(arr[index]);
+        temp->next=CreateLL(arr,index+1);
+        
+        return temp;
+}
+int main(){
+    Node *Head=NULL;
+
+    vector<int>arr={4,6,9,3};
+
+    Head=CreateLL(arr,0);
+
+     /* Print the linked List */
         Node *temp=Head;
         while(temp!=NULL){
-            cout<<temp->data<<" ";
+            cout<<temp->data<<"|";
+            cout<<temp->next<<"| -> |";
             temp=temp->next;
         }
-return 0 ;
+    return 0;
 }
-
