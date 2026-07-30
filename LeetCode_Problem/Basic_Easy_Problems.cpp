53ba07f67bbdfb93aa435d35fb83ea654a85678a
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-07-30 23:21:03 +0530

    commit

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -2659,16 +2659,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n=nums.size();
-        for(int i=0;i<n-1;i++){
-                for(int j=i+1;j<n;j++){
-                        if(nums[i]==nums[j]){
-                                if(abs(i-j)<=k){
-                                    return true;    
-                                }
-                        }
-                }
+        unordered_map<int, int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(nums[i]) != mp.end()) {
+                if (i - mp[nums[i]] <= k)
+                    return true;
+            }
+            mp[nums[i]] = i;
         }
+
         return false;
 }
 int main(){
