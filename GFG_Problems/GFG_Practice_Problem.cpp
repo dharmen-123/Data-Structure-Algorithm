f036eca82313d420f7f33c4df326f63a326f184e
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-01 20:13:53 +0530

    Queue basics

--- GFG_Problems/GFG_Practice_Problem.cpp
+++ GFG_Problems/GFG_Practice_Problem.cpp
@@ -937,71 +937,74 @@
 
                 /* // Q.Get Min from Stack // */
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-class SpecialStack {
-	stack<int>st1;
-	stack<int>st2;
-	public:
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// class SpecialStack {
+// 	stack<int>st1;
+// 	stack<int>st2;
+// 	public:
 	
-	void push(int x) {
-		// Add an element to the top of Stack
-		st1.push(x);
-		if (st2.empty())
-			st2.push(x);
-		else
-			st2.push(min(st2.top(), x));
-	}
+// 	void push(int x) {
+// 		// Add an element to the top of Stack
+// 		st1.push(x);
+// 		if (st2.empty())
+// 			st2.push(x);
+// 		else
+// 			st2.push(min(st2.top(), x));
+// 	}
 	
-	void pop() {
-		// Remove the top element from the Stack
-		if (!st1.empty()) {
-			st1.pop();
-			st2.pop();
-		}
-	}
+// 	void pop() {
+// 		// Remove the top element from the Stack
+// 		if (!st1.empty()) {
+// 			st1.pop();
+// 			st2.pop();
+// 		}
+// 	}
 	
-	int peek() {
-		// Returns top element of the Stack
-		if (!st1.empty()) {
-			return st1.top();
-		}
-	    return -1;
-	}
+// 	int peek() {
+// 		// Returns top element of the Stack
+// 		if (!st1.empty()) {
+// 			return st1.top();
+// 		}
+// 	    return -1;
+// 	}
 	
-	bool isEmpty() {
-		// Check if stack is empty
-			return st1.empty();
-	}
+// 	bool isEmpty() {
+// 		// Check if stack is empty
+// 			return st1.empty();
+// 	}
 	
-	int getMin() {
-		// Finds minimum element of Stack
-		if (st2.empty()) {
-			return - 1;
-		}
-		else {
-			return st2.top();
-		}
-	}
-};
-int main(){
-    SpecialStack st;
-    st.push(2);
-    st.push(3);
-    cout << "Top = " << st.peek() << endl;
-    cout << "Min = " << st.getMin() << endl;
-    st.pop();
-    cout << "Top = " << st.peek() << endl;
-    cout << "Min = " << st.getMin() << endl;
-    st.push(1);
-    cout << "Top = " << st.peek() << endl;
-    cout << "Min = " << st.getMin() << endl;
-    st.pop();
-    st.pop();
-    cout << "Is Empty = " << st.isEmpty() << endl;
-    cout << "Top = " << st.peek() << endl;
-    cout << "Min = " << st.getMin() << endl;
+// 	int getMin() {
+// 		// Finds minimum element of Stack
+// 		if (st2.empty()) {
+// 			return - 1;
+// 		}
+// 		else {
+// 			return st2.top();
+// 		}
+// 	}
+// };
+// int main(){
+//     SpecialStack st;
+//     st.push(2);
+//     st.push(3);
+//     cout << "Top = " << st.peek() << endl;
+//     cout << "Min = " << st.getMin() << endl;
+//     st.pop();
+//     cout << "Top = " << st.peek() << endl;
+//     cout << "Min = " << st.getMin() << endl;
+//     st.push(1);
+//     cout << "Top = " << st.peek() << endl;
+//     cout << "Min = " << st.getMin() << endl;
+//     st.pop();
+//     st.pop();
+//     cout << "Is Empty = " << st.isEmpty() << endl;
+//     cout << "Top = " << st.peek() << endl;
+//     cout << "Min = " << st.getMin() << endl;
 
-return 0 ;
-}
+// return 0 ;
+// }
+
+
+                /* // Q.  // */
