3a00ed3c9b46f9ed0f0e5613465044277355e392
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-05 23:04:19 +0530

    Circular Queue

--- QUEUE/Queue_Implementation.cpp
+++ QUEUE/Queue_Implementation.cpp
@@ -35,6 +35,80 @@
 
         /* Queue Implementation Using Array */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Queue{
+//     int *arr;
+//     int front, rear;
+//     int size;
+//     public:
+//         Queue(int n){
+//             arr=new int[n];
+//             front = -1 , rear = -1;
+//             size=n;
+//         }
+//      void push(int val){
+//         if(Isempty()){
+//             front=rear=0;
+//             arr[rear]=val;
+//         }
+//         else if(Isfull()){
+//             cout<<"Queue is Overflow"<<endl;
+//             return ;
+//         }
+//         else{
+//             rear+=1;
+//             arr[rear]=val;
+//             cout<<val<<" is insert in queue"<<endl;
+//         }
+//      } 
+//      void pop(){
+//         if(Isempty()){
+//             cout<<"Queue Underflow"<<endl;
+//             return ;
+//         }
+//         else{
+//             if(front==rear){
+//                 front=rear=-1;
+//             }
+//             else{
+//             cout<<arr[front]<<" is removed from queue"<<endl;
+//             front+=1;
+//             }
+//         }
+//      }
+//      int start(){
+//         if(Isempty()){
+//             cout<<"Queue is Empty"<<endl;
+//             return -1;
+//         }
+//         else{
+//             return arr[front];
+//         }
+//      }
+//      bool Isfull(){
+//         return rear==size-1;
+//      }
+//      bool Isempty(){
+//         return front==-1;
+//      }
+
+// };
+// int main(){
+//         Queue q(5);
+//         q.push(10);
+//         q.push(8);
+//         q.push(23);
+//         q.pop();
+//         cout<<q.Isfull()<<endl;
+//         cout<<q.start()<<endl;
+
+// return 0 ;
+// }
+
+        /* Circular Queue Implementation using with Arrzy*/
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -58,7 +132,7 @@ class Queue{
             return ;
         }
         else{
-            rear+=1;
+            rear=(rear+1)%size;
             arr[rear]=val;
             cout<<val<<" is insert in queue"<<endl;
         }
@@ -74,7 +148,7 @@ class Queue{
             }
             else{
             cout<<arr[front]<<" is removed from queue"<<endl;
-            front+=1;
+            front=(front+1)%size;
             }
         }
      }
@@ -88,7 +162,7 @@ class Queue{
         }
      }
      bool Isfull(){
-        return rear==size-1;
+        return (rear+1)%size==front;
      }
      bool Isempty(){
         return front==-1;
@@ -103,6 +177,13 @@ int main(){
         q.pop();
         cout<<q.Isfull()<<endl;
         cout<<q.start()<<endl;
+        q.push(65);
+        q.pop();
+        q.push(76);
+        q.push(3);
+        q.push(5);
+        cout<<q.Isfull()<<endl;
 
 return 0 ;
 }
+
