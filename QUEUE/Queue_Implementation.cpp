2d9bd991bd4f9f0842bd1ea12bbf63b40baa2f0a
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-05 20:43:17 +0530

    Queue implement using Array

--- QUEUE/Queue_Implementation.cpp
+++ QUEUE/Queue_Implementation.cpp
@@ -33,3 +33,76 @@
 // }
 
 
+        /* Queue Implementation Using Array */
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Queue{
+    int *arr;
+    int front, rear;
+    int size;
+    public:
+        Queue(int n){
+            arr=new int[n];
+            front = -1 , rear = -1;
+            size=n;
+        }
+     void push(int val){
+        if(Isempty()){
+            front=rear=0;
+            arr[rear]=val;
+        }
+        else if(Isfull()){
+            cout<<"Queue is Overflow"<<endl;
+            return ;
+        }
+        else{
+            rear+=1;
+            arr[rear]=val;
+            cout<<val<<" is insert in queue"<<endl;
+        }
+     } 
+     void pop(){
+        if(Isempty()){
+            cout<<"Queue Underflow"<<endl;
+            return ;
+        }
+        else{
+            if(front==rear){
+                front=rear=-1;
+            }
+            else{
+            cout<<arr[front]<<" is removed from queue"<<endl;
+            front+=1;
+            }
+        }
+     }
+     int start(){
+        if(Isempty()){
+            cout<<"Queue is Empty"<<endl;
+            return -1;
+        }
+        else{
+            return arr[front];
+        }
+     }
+     bool Isfull(){
+        return rear==size-1;
+     }
+     bool Isempty(){
+        return front==-1;
+     }
+
+};
+int main(){
+        Queue q(5);
+        q.push(10);
+        q.push(8);
+        q.push(23);
+        q.pop();
+        cout<<q.Isfull()<<endl;
+        cout<<q.start()<<endl;
+
+return 0 ;
+}
