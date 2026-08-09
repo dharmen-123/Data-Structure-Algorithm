165742293b012b11a4f6060e1a5ee5c3c5672d45
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-09 20:35:43 +0530

    Print all number in every window size k

--- QUEUE/Questions_on_Queue.cpp
+++ QUEUE/Questions_on_Queue.cpp
@@ -57,41 +57,83 @@
 
             /* Reverse the Queue Elemenets using Stack  */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     queue<int>q;
+//     q.push(2);
+//     q.push(4);
+//     q.push(6);
+//     q.push(8);
+//     q.push(10);
+    
+//     stack<int>s;
+//     /* Insert the element into stack */
+//     while(!q.empty()){
+//         s.push(q.front());
+//         cout<<q.front()<<" ";
+//         q.pop();
+//     }
+
+//     /* Pop elemenet from stack and insert into queue */
+//     while(!s.empty()){
+//         q.push(s.top());
+//         s.pop();
+//     }
+
+//     cout<<"\nAfter the reverse the Queue element"<<endl;
+//     int n=q.size();
+//     /* print all the element */
+//     while(n--){
+//         cout<<q.front()<<" ";
+//         int val=q.front();
+//         q.pop();
+//         q.push(val);
+//     }
+
+// return 0 ;
+// }
+
+            /* Print all number in every window size k */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-int main(){
-    queue<int>q;
-    q.push(2);
-    q.push(4);
-    q.push(6);
-    q.push(8);
-    q.push(10);
-    
-    stack<int>s;
-    /* Insert the element into stack */
+vector<int> storewindow(queue<int>q){
+    vector<int>ans;
     while(!q.empty()){
-        s.push(q.front());
-        cout<<q.front()<<" ";
+        ans.push_back(q.front());
         q.pop();
     }
-
-    /* Pop elemenet from stack and insert into queue */
-    while(!s.empty()){
-        q.push(s.top());
-        s.pop();
-    }
-
-    cout<<"\nAfter the reverse the Queue element"<<endl;
-    int n=q.size();
-    /* print all the element */
-    while(n--){
-        cout<<q.front()<<" ";
-        int val=q.front();
-        q.pop();
-        q.push(val);
-    }
-
+    return ans;
+}
+vector<vector<int>> PrintNumberinwindow(vector<int>&arr,int k){
+       queue<int>q;
+       vector<int>a;
+       vector<vector<int>>ans;
+       for(int i=0;i<k-1;i++){
+           q.push(arr[i]);
+       }
+       for(int i=k-1;i<arr.size();i++){
+          q.push(arr[i]);
+          a=storewindow(q);
+          ans.push_back(a);
+          a.clear();  
+          q.pop();
+       }
+       return ans;
+} 
+int main(){
+        vector<int>arr={3,6,2,7,8,11};
+        int k=3;
+        vector<vector<int>>ans=PrintNumberinwindow(arr,k);
+        for(auto k:ans){
+            for(auto a:k){
+                cout<<a<<" ";
+            }
+            cout<<endl;
+        }
 return 0 ;
 }
