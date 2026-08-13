dee093131467d497616306d9dcd9c6166ef942bd
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-13 22:59:56 +0530

    commit

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -3039,25 +3039,25 @@
 
                 /* // Q.387. First Unique Character in a String // */
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int firstUniqChar(string s) {
-        unordered_map<char,int>freq;
-        for(auto x:s){
-              freq[x]++;  
-        }
-        for(int i=0;i<s.size();i++){
-              if(freq[s[i]]==1){
-                   return i;              
-              }  
-        }
-       return -1; 
-}
-int main(){
-        string s="leetcode";
-        // string s="aabb";
-        cout<<firstUniqChar(s);
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int firstUniqChar(string s) {
+//         unordered_map<char,int>freq;
+//         for(auto x:s){
+//               freq[x]++;  
+//         }
+//         for(int i=0;i<s.size();i++){
+//               if(freq[s[i]]==1){
+//                    return i;              
+//               }  
+//         }
+//        return -1; 
+// }
+// int main(){
+//         string s="leetcode";
+//         // string s="aabb";
+//         cout<<firstUniqChar(s);
 
-return 0 ;
-}
+// return 0 ;
+// }
