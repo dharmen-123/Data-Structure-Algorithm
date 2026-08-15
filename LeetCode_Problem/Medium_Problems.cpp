73f62409ff48fb8b768d6cda47d672917f87632d
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-08-15 20:38:25 +0530

    commit

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -2349,55 +2349,70 @@
 
         /* // Q.92. Reverse Linked List II // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// class ListNode {
+//     public:
+//      int val;
+//      ListNode *next;
+//      ListNode() : val(0), next(nullptr) {}
+//      ListNode(int x) : val(x), next(nullptr) {}
+//      ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+// ListNode* reverseBetween(ListNode* head, int left, int right) {
+//         if(head == NULL || left == right)
+//         return head;
+//         ListNode* dummy = new ListNode(0);
+//         dummy->next = head;
+//         ListNode* prev = dummy;
+//         for(int i = 1; i < left; i++) {
+//             prev = prev->next;
+//         }
+//         ListNode* curr = prev->next;
+//         for(int i = 0; i < right - left; i++) {
+//             ListNode* temp = curr->next;
+//             curr->next = temp->next;
+//             temp->next = prev->next;
+//             prev->next = temp;
+//         }
+//         return dummy->next;
+
+//     }
+// int main(){
+//         ListNode *head =new ListNode(1);
+//         ListNode *n2 =new ListNode(2);
+//         ListNode *n3 =new ListNode(3);
+//         ListNode *n4 =new ListNode(4);
+//         ListNode *n5 =new ListNode(5);
+//         head->next=n2;
+//         n2->next=n3;
+//         n3->next=n4;
+//         n4->next=n5;
+//         int left=2 ,right=4;
+//         head=reverseBetween(head,left,right);
+
+//        /* Print the linked List */
+//         ListNode *temp=head;
+//         while(temp!=NULL){
+//             cout<<temp->val<<" ";
+//             temp=temp->next;
+//         }
+
+// return 0 ;
+// }
+
+        /* // Q.Q2. Minimum Operations to Make a Rotated Palindrome I // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-class ListNode {
-    public:
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(head == NULL || left == right)
-        return head;
-        ListNode* dummy = new ListNode(0);
-        dummy->next = head;
-        ListNode* prev = dummy;
-        for(int i = 1; i < left; i++) {
-            prev = prev->next;
-        }
-        ListNode* curr = prev->next;
-        for(int i = 0; i < right - left; i++) {
-            ListNode* temp = curr->next;
-            curr->next = temp->next;
-            temp->next = prev->next;
-            prev->next = temp;
-        }
-        return dummy->next;
-
-    }
+int minOperations(string s) {
+    
+    
+}
 int main(){
-        ListNode *head =new ListNode(1);
-        ListNode *n2 =new ListNode(2);
-        ListNode *n3 =new ListNode(3);
-        ListNode *n4 =new ListNode(4);
-        ListNode *n5 =new ListNode(5);
-        head->next=n2;
-        n2->next=n3;
-        n3->next=n4;
-        n4->next=n5;
-        int left=2 ,right=4;
-        head=reverseBetween(head,left,right);
 
-       /* Print the linked List */
-        ListNode *temp=head;
-        while(temp!=NULL){
-            cout<<temp->val<<" ";
-            temp=temp->next;
-        }
 
 return 0 ;
 }
