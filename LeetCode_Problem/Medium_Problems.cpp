f63b5c847f37e8901e32f367de0f750e93550ad9
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-09-03 21:57:23 +0530

    commit

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -2704,33 +2704,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool uniformArray(vector<int>& nums1) {
-        int n=nums1.size();
-        int count=0;
-        if(n==1)
-            return true; 
-        for(auto k:nums1){
-               if(k%2!=0){
-                  count++;
-               } 
-        }       
-        if(count==0 || count==n)
-             return true;       
-        if (n == 2) {
-            int a = nums1[0], b = nums1[1];
-            if ((a % 2 == b % 2)) 
-                return true;
-            if ((a - b) >= 1 && ((a - b) % 2 == a % 2)) 
-                return true;
-            if ((b - a) >= 1 && ((b - a) % 2 == b % 2)) 
-                return true;
-          return false;
-        }
+        int mn = nums1[0];
+    bool allEven = true;
 
+    for (int x : nums1) {
+        if (x < mn)
+            mn = x;
+
+        if (x % 2 != 0)
+            allEven = false;
+    }
+
+    if (mn % 2 != 0)
         return true;
+
+    return allEven;
 }
 int main(){
         // vector<int>nums1={1,4,7};
-        vector<int>nums1={5,3,2,1,6};
+        vector<int>nums1={11,16};
         cout<<uniformArray(nums1);
 
 return 0 ;
