bea54d52bf509cdbb67a726654ea2f7f6d589c9c
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-09-04 23:51:40 +0530

    commit

--- LeetCode_Problem/Basic_Easy_Problems.cpp
+++ LeetCode_Problem/Basic_Easy_Problems.cpp
@@ -3692,37 +3692,37 @@
 
         /* // Q.3903. Smallest Stable Index I // */
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int firstStableIndex(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int>largest;
-        vector<int>smallest;
-        int minv=INT_MAX;
-        int maxv=INT_MIN;
-        for(auto k:nums){
-             maxv=max(k,maxv);  
-             largest.push_back(maxv);
-        }
-        for(int i=n-1;i>=0;i--){
-             minv=min(nums[i],minv);   
-             smallest.push_back(minv);  
-        }
-        reverse(smallest.begin(),smallest.end());
-        for(int i=0;i<n;i++){
-             if((largest[i]-smallest[i])<=k){
-                return i;
-             }   
-        }
-        return -1;
-}
-int main(){
-        vector<int>nums={5,0,1,4};
-        int k=3;
-        // vector<int>nums={3,2,1};
-        // int k=1;
-        cout<<firstStableIndex(nums,k);
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int firstStableIndex(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         vector<int>largest;
+//         vector<int>smallest;
+//         int minv=INT_MAX;
+//         int maxv=INT_MIN;
+//         for(auto k:nums){
+//              maxv=max(k,maxv);  
+//              largest.push_back(maxv);
+//         }
+//         for(int i=n-1;i>=0;i--){
+//              minv=min(nums[i],minv);   
+//              smallest.push_back(minv);  
+//         }
+//         reverse(smallest.begin(),smallest.end());
+//         for(int i=0;i<n;i++){
+//              if((largest[i]-smallest[i])<=k){
+//                 return i;
+//              }   
+//         }
+//         return -1;
+// }
+// int main(){
+//         vector<int>nums={5,0,1,4};
+//         int k=3;
+//         // vector<int>nums={3,2,1};
+//         // int k=1;
+//         cout<<firstStableIndex(nums,k);
 
-return 0 ;
-}
+// return 0 ;
+// }
