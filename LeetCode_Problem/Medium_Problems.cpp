0b903ecc0d4fd5d062e7102969eb617c520f391b
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-09-12 20:17:12 +0530

    Count Values With Equally Spaced Occurrences II

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -2879,19 +2879,57 @@
 
         /* // Q.3871. Count Commas in Range II  // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// long long countCommas(long long n) {
+//         long long ans = 0;
+//         for(long long x = 1000; x <= n; x *= 1000) {
+//             ans += n - x + 1;
+//         }
+//         return ans;
+// }
+// int main(){
+//         long long n=1004590;
+//         cout<<countCommas(n);
+
+// return 0 ;
+// }
+
+        /* // Q.4049 Count Values With Equally Spaced Occurrences II // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-long long countCommas(long long n) {
-        long long ans = 0;
-        for(long long x = 1000; x <= n; x *= 1000) {
-            ans += n - x + 1;
+int countSpecialIntegers(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,vector<int>>freq;
+        for(int i=0;i<n;i++){
+                freq[nums[i]].push_back(i);
         }
-        return ans;
+        int ans=0;
+        for(auto x:freq){
+             if(x.second.size()>=3){
+                bool check=true;
+                int gap=x.second[1]-x.second[0];
+                for(int i=1;i<x.second.size();i++){
+                       if(!(x.second[i]-x.second[i-1]==gap)){
+                               check=false;                   
+                       } 
+                 }
+                 if(check){
+                        ans++;
+                 }
+             }  
+        }
+    return ans;
+
 }
 int main(){
-        long long n=1004590;
-        cout<<countCommas(n);
+        vector<int>nums={1,8,1,5,1,5,8,5};
+        // vector<int>nums={8,8,8,8};
+        cout<<countSpecialIntegers(nums);
+
 
 return 0 ;
 }
