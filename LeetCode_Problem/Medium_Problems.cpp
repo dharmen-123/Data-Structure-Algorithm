08508494ca9186cd9e09bd27ad147bf371c73275
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-09-21 23:32:17 +0530

    Find X Value of Array I

--- LeetCode_Problem/Medium_Problems.cpp
+++ LeetCode_Problem/Medium_Problems.cpp
@@ -3008,22 +3008,57 @@
 
         /* // Q.1401. Circle and Rectangle Overlapping  // */
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2){
+//         int closestX = max(x1, min(xCenter, x2));
+//         int closestY = max(y1, min(yCenter, y2));
+//         long long dx = xCenter - closestX;
+//         long long dy = yCenter - closestY;
+
+//      return dx * dx + dy * dy <= 1LL * radius * radius;
+
+// }
+// int main(){
+//         int radius = 1, xCenter = 0, yCenter = 0, x1 = 1, y1 = -1, x2 = 3, y2 = 1;
+//         // int radius = 1, xCenter = 1, yCenter = 1, x1 = 1, y1 = -3, x2 = 2, y2 = -1;
+//         cout<<checkOverlap(radius,xCenter,yCenter,x1,y1,x2,y2);
+
+// return 0 ;
+// }
+
+        /* // Q.3524. Find X Value of Array I // */
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2){
-        int closestX = max(x1, min(xCenter, x2));
-        int closestY = max(y1, min(yCenter, y2));
-        long long dx = xCenter - closestX;
-        long long dy = yCenter - closestY;
+vector<long long> resultArray(vector<int>& nums, int k) {
+       vector<long long> ans(k, 0);
+       vector<long long> dp(k, 0);
+       for (int num : nums) {
+           vector<long long> ndp(k, 0);
+           int rem = num % k;
+           ndp[rem]++;
+           for (int r = 0; r < k; r++) {
+               int newRem = (r * rem) % k;
+               ndp[newRem] += dp[r];
+           }
+           for (int r = 0; r < k; r++) {
+               ans[r] += ndp[r];
+           }
+   
+           dp = ndp;
+       }
 
-     return dx * dx + dy * dy <= 1LL * radius * radius;
-
+    return ans;
 }
 int main(){
-        int radius = 1, xCenter = 0, yCenter = 0, x1 = 1, y1 = -1, x2 = 3, y2 = 1;
-        // int radius = 1, xCenter = 1, yCenter = 1, x1 = 1, y1 = -3, x2 = 2, y2 = -1;
-        cout<<checkOverlap(radius,xCenter,yCenter,x1,y1,x2,y2);
-
+        vector<int>nums = {1,2,3,4,5};
+        int k = 3;
+        vector<long long>ans=resultArray(nums,k);
+        for(auto k:ans){
+               cout<<k<<" "; 
+        }
 return 0 ;
 }
